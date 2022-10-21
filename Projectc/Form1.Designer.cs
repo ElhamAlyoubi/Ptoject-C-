d8ae2dfc7ebb563f51865af51ac8978f9348ca39
ElhamAlyoubi <elhm1020@gmail.com>
2022-10-22 01:29:36 +0300

    hi]

--- Projectc/Form1.Designer.cs
+++ Projectc/Form1.Designer.cs
@@ -43,12 +43,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(245, 97);
+            this.button2.Location = new System.Drawing.Point(503, 96);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- Projectc/Form1.cs
+++ Projectc/Form1.cs
@@ -16,5 +16,10 @@ namespace Projectc
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
